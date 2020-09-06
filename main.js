bdf5831591ee0611fa20218a3a45024fdc48b522
Juan Carlos del Rio <jcdre@hotmail.com>
2020-09-06 18:29:43 -0500

    Finished desktop view

--- main.js
+++ main.js
@@ -6,8 +6,33 @@ window.onload = () => {
 
 /** Esta funcion se llama para abrir el modal */
 function openModal(e) {
-  document.querySelector(".contact").style.display = "none";
-  document.querySelector(".share-container").style.display = "grid";
+
+  const elem = document.querySelector(".share").getBoundingClientRect();
+  console.log("elem: ", elem);
+  
+  
+  if(window.innerWidth>=768){
+    if(document.querySelector(".share-container2").style.display == "grid"){
+      document.querySelector(".share-container2").style.display = "none";
+      document.querySelector(".share").style.backgroundColor = "hsl(210, 46%, 95%)";
+      document.querySelector(".img-share").src = "./images/icon-share.svg";
+      // console.log(document.querySelector(".share-container2").style.left);
+      // console.log(elem.left);
+      
+    }
+    else{
+      document.querySelector(".share-container2").style.display = "grid";
+      const elem2 = document.querySelector(".share-container2").getBoundingClientRect();
+      document.querySelector(".share-container2").style.left = (elem.left - elem2.width/2) + "px";
+      document.querySelector(".share-container2").style.top = (elem.top - elem2.height) + "px";
+      document.querySelector(".share").style.backgroundColor = "hsl(217, 19%, 35%)";
+      document.querySelector(".img-share").src = "./images/icon-shareW.svg";
+    }
+  }else{
+    document.querySelector(".contact").style.display = "none";
+    document.querySelector(".share-container").style.display = "grid";
+  }
+
 }
 
 /** Esta funcion se llama para cerrar el modal */
